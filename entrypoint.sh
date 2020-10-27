1259b50c3650769e2dbf8c43f2009e253bf9968f
Martin.intrising <martin@intrising.com.tw>
2020-10-27 21:24:18 +0800

    fix: add install script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,14 @@ INPUT_BRANCH=${INPUT_BRANCH:-master}
 
 set -e
 
+echo "Installing requirements..."
+if [[ -f "requirements.txt" ]]; then
+    # Ensure commitizen + reqs may have custom commitizen plugins
+    pip install -r requirements.txt commitizen
+else
+    pip install commitizen
+fi
+
 echo "Running cz check $INPUT_BRANCH"
 cz check --rev-range $INPUT_BRANCH
 
